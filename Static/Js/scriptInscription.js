c4326247eff3157d0ed8ba8167be4bb1ce16b3d3
Jean-Charles Leflamand <jeancharles2403@gmail.com>
2023-03-28 16:59:43 +0200

    debut js

---
+++ Static/Js/scriptInscription.js
@@ -0,0 +1,8 @@
+const url = "https://afpatraining.snage.tech/docs/#/Pages/signup";
+const apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2Nzk0Nzg2MjAsImV4cCI6MTY3OTQ3ODY4MH0.ZPhc7O7KJnEpeXLTKz1PNEWUqB_mFA4XefUFIEXWrGE";
+const pseudo = document.getElementById("#pseudo").value;
+const eMail = document.getElementById("#eMail").value;
+const motDePasse = document.getElementById("#motDePasse").value;
+const valideMotDePasse = document.getElementById("#validationDeMotDePasse").value;
+
+let requete = new XMLHttpRequest();
